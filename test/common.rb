79483a3975f3fdd426536b717e4ddc50f7579012
Kurt Yoder <kyoder@loc.gov>
2013-06-11 18:10:45 -0400

    Move "Comparison*" test code into its own file

--- test/common.rb
+++ test/common.rb
@@ -3,3 +3,20 @@ require 'rubygems'
 $:.unshift(File.dirname(__FILE__) + '/../lib') unless $:.include?('/../lib')
 require 'iptables'
 require 'test/unit'
+
+# inspired by https://github.com/cespare/ruby-dedent
+class String
+	def dedent
+		lines = split "\n"
+		return self if lines.empty?
+		# first indented line determines indent level
+		indentation = nil
+		lines.each{ |line|
+			next unless line =~ /^(\s+)/
+			indentation = $1
+			break
+		}
+		return self if indentation.nil?
+		lines.map { |line| line.sub(/^#{indentation}/, "") }.join "\n"
+	end
+end
